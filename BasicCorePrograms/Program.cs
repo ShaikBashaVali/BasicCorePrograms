69987d4467c64e44c0c7921c736d860846446aa8
BashaVali <bashavali222@gmail.com>
2023-02-26 23:07:43 +0530

    Basic Copre Programs

---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,36 @@
+﻿using BasicPrograms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class Flip
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Basic Core Programs");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select the option");
+                Console.WriteLine("1.FlipCoin" + "\n" +
+                                  "2.Exit" + "\n");
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        FlipCoin flipCoin = new FlipCoin();
+                        flipCoin.Flip();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+
+                }
+            }
+        }
+    }
+}
